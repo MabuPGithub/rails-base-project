99a300ccd2fa36ad143af6da95076947a7e17486
MabuPGithub <mylespison@yahoo.com>
2021-08-02 19:24:31 +0800

    fixed confirmable user email. edited site title. fixed admin migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users, :controllers => { 
     registrations: 'users/registrations',
     sessions: 'users/sessions'
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :admins
+  
 end
