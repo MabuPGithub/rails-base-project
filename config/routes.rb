c30b3302d9b40ed29a9d933029508009de6e14fa
MabuPGithub <mylespison@yahoo.com>
2021-08-06 18:52:42 +0800

    added admin module. admin dashboard, approve user fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { 
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
+  devise_for :admins, :controllers => { registrations: 'admins/registrations', sessions: "admins/sessions"}
+  devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
+  
+  get '/get/stock-view' => 'stocks_view#index', as: 'stock_view_index'
+  get '/get/stock-view/:stocksymbol' => 'stocks_view#show', as: 'stock_view_show' 
+  get 'admins/pending_show' => 'admins#pending_show', as: 'admin_pending_show'
+  post 'admins/pending_approve' => 'admins#pending_approve', as: 'admin_pending_approve'
+  get 'admins/create_new_user' => 'admins#create_new_user', as: 'admin_create_new_user'
+  # get 'admins/all_transactions' => 'admins#all_transactions', as: 'admin_all_transactions'
+  get "/users", to: "stocks_view#index", :as => :user_root
+  get "/admins", to: "admins#index", :as => :admin_root
 
-  devise_for :admins, path: 'admins', controllers: {
-    sessions: 'admins/sessions',
-    registrations: 'admins/registrations'
-  }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :admins
-  
 end
